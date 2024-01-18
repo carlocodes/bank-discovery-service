fd4a89df76921cdefa9c5305887b29883ae1baa7
carlocodes <Gian Uy>
2024-01-18 20:20:14 +0800

    initial commit

---
+++ src/main/java/com/carlocodes/bankdiscoveryservice/BankDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.carlocodes.bankdiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlocodes/bankdiscoveryservice/BankDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlocodes.bankdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
